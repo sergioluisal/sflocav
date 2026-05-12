54f9a8d5be29dd23cbd4b5d5cbc33db98d435a5c
sergioluisal <sergioluisal@gmail.com>
2026-05-12 11:55:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ if page == "Início":
         "Semanal": ["R$ 700,00"],
         "Calção": ["R$ 1.700,00"]
     }
-    st.table(planos_data)
+    st.table(color: white; planos_data)
     
     col_cta_left, col_cta_center, col_cta_right = st.columns([1, 2, 1])
     with col_cta_center:
